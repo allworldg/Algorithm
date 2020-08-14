38ac49d19952860909458127c50f904962bb8d73
allworldg <944937881@qq.com>
2020-08-14 16:47:38 +0800

    merge_sort

---
+++ Sort/Merge_sort/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 10};
+        nums = mergesort(nums, 0, nums.length - 1);
+        Utils.printout(nums);
+    }
+
+    /**
+     * 归并排序（merge_sort）
+     *
+     * @param arr 需要排序的数组 从小到大
+     * @param l   数组左侧    left
+     * @param r   数组右侧    right
+     * @return
+     */
+    static int[] mergesort(int[] arr, int l, int r) {
+        if (l == r) {
+            return new int[]{arr[l]};
+        }
+        int mid = (l + r) / 2;
+        int[] leftArr = mergesort(arr, l, mid); //左侧有序数组
+        int[] rightArr = mergesort(arr, mid + 1, r); //右侧有序数组
+        int[] newArr = new int[leftArr.length + rightArr.length]; //新的有序数组
+
+        int m = 0, i = 0, j = 0;
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] < rightArr[j]) {
+                newArr[m++] = leftArr[i++];
+            } else {
+                newArr[m++] = rightArr[j++];
+            }
+        }
+
+            while (j < rightArr.length) {
+                newArr[m++] = rightArr[j++];
+            }
+
+            while (i < leftArr.length) {
+                newArr[m++] = leftArr[i++];
+            }
+
+        return newArr;
+    }
+}
---
+++ Sort/Merge_sort/src/Utils.java
@@ -0,0 +1,49 @@
+/**
+ * 工具类
+ */
+public class Utils {
+    /**
+     * 整型交换方法，返回数组
+     *
+     * @param arr 数组
+     * @param a   下标
+     * @param b   下标
+     * @return
+     */
+    static int[] swap(int arr[], int a, int b) {
+        int temp;
+        temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
+
+
+
+    /**
+     * 用于将其他位置的值插入到指定地方
+     * @param arr
+     * @param a 需要插入的目标位置
+     * @param b 要移动的下标
+     * @return 改变后的数组
+     */
+    static int[] insert(int arr[], int a, int b) {
+        int temp;
+        temp = arr[b];
+        for(int i=b;i>a;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[a] = temp;
+        return arr;
+    }
+
+    /**
+     * 循环打印数组的每个值
+     * @param arr
+     */
+    static void printout(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
