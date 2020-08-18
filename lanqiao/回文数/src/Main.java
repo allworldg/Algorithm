5f8f02c229afc5b81058808bf657010904770f2d
allworldg <944937881@qq.com>
2020-08-18 16:30:20 +0800

    radix change

---
+++ lanqiao/回文数/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @Classname Main
+ * @Description TODO
+ * @Date 2020/8/18 16:29
+ * @Created by 94493
+ */
+public class Main {
+}
--- lanqiao/进制转换/src/Main.java
+++ lanqiao/进制转换/src/Main.java
@@ -9,16 +9,37 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        sc.nextLine();//此处是为了不让下面的nextline读取到上面nextint的回车
+//
+//        for (int i = 1; i <= n; i++) {
+//            String result = new BigInteger(sc.nextLine(),16).toString(8);
+//            System.out.println(result);
+
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();//此处是为了不让下面的nextline读取到上面nextint的回车
-
-        for (int i = 1; i <= n; i++) {
-            String result = new BigInteger(sc.nextLine(),16).toString(8);
-            System.out.println(result);
-        }
+        decimaltohex(sc.nextInt());
+    }
 
 
+    /**
+     * 十六进制转换成十进制
+     *
+     * @param n
+     */
+    public static void hextodecimal(String n) {
+        String result = new BigInteger(n, 16).toString(10);
+        System.out.println(result);
+
+    }
+
+    /**
+     * 十进制转换十六进制
+     * @param n
+     */
+    public static void decimaltohex(int n){
+        String result = Integer.toHexString(n).toUpperCase();
+        System.out.println(result);
     }
 
 }
